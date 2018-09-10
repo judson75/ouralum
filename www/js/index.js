2e5fef814de677a631389d48e42fcfcdc2ed595e
Judson Cooper <jcooper@hightowerservices.com>
2018-09-10 14:19:11 -0500

    Initial Push

---
+++ www/js/index.js
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+		//var myContact = navigator.contacts.create({"displayName": "Test User"});
+        //myContact.note = "This contact has a note.";
+        //alert("The contact, " + myContact.displayName + ", note: " + myContact.note);
+		var options = new ContactFindOptions();
+		options.multiple = true;
+        options.filter = "";
+        var filter = ["displayName", "addresses", "emails", "phoneNumbers"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+
+    },
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "XXXXXXXX"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+
+        push.on('registration', function(data) {
+            console.log('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+       });
+    }
+};
+
+function onSuccess(contacts) {
+	var contacts_html = '';
+	for (var i = 0; i < contacts.length; i++) {
+		contacts_html += contacts[i].displayName;
+		if(contacts[i].emails !== null) {
+			contacts_html += contacts[i].emails[0].value + "<br>\n";	
+		}
+		if(contacts[i].phoneNumbers !== null) {
+			contacts_html += contacts[i].phoneNumbers[0].value + "<br>\n";	
+		}
+	}
+	var parentElement = document.getElementById('registration');
+	var receivedElement = parentElement.querySelector('.received');
+	document.getElementById('registration').innerHTML = contacts_html;
+}
+
+// onError: Failed to get the contacts
+//
+function onError(contactError) {
+	alert('onError!');
+}
